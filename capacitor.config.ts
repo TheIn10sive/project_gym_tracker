53618f51e718efe3089e10e5a1502f18d0ac1fba
Pit Schmahl <pit.schmahl@gmail.com>
2022-10-25 12:12:28 +0200

    init commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gym.log',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
